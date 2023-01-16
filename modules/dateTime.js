6aa032db6ab8058332eb42b1fe36a70dca4e9c45
cindykandie <cindyjk6@gmail.com>
2023-01-16 19:32:02 +0300

    add ES6 and luxonJS time

---
+++ modules/dateTime.js
@@ -0,0 +1,17 @@
+import { DateTime } from './luxon.js';
+
+const displayTime = () => {
+  const timing = DateTime.local();
+  const newDate = timing.toLocaleString({
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+  });
+
+  const newTime = timing
+    .toLocaleString(DateTime.TIME_WITH_SECONDS);
+
+  document.querySelector('.time').textContent = `${newDate}, ${newTime}`;
+};
+
+export default displayTime();
